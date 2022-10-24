ac3623167349178a44ba18593d4082139227d577
Vasilije Dikic <vaske001@gmail.com>
2022-10-24 13:55:36 +0200

    sprint 3

--- springboot/src/main/java/at/fhtw/swen3/persistence/entity/ParcelEntity.java
+++ springboot/src/main/java/at/fhtw/swen3/persistence/entity/ParcelEntity.java
@@ -6,32 +6,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
 public class ParcelEntity {
-    @Id
-    @Pattern(regexp = "^[A-Z0-9]{9}$")
     private String trackingId;
-    @NotNull(message = "Weight cannot be null")
-    @Min(value = 0, message = "Weight cannot be negative")
     private Float weight;
-    @NotNull(message = "Recipient ID cannot be null")
     private Long recipient;
-    @NotNull(message = "Sender ID cannot be null")
     private Long sender;
-    @NotNull(message = "State cannot be null")
     private TrackingInformation.StateEnum state;
-    @NotNull(message = "Visited Hops cannot be null")
     private List<Long> visitedHops;
-    @NotNull(message = "Future Hops cannot be null")
     private List<Long> futureHops;
 }
--- springboot/src/main/java/at/fhtw/swen3/persistence/entity/RecipientEntity.java
+++ springboot/src/main/java/at/fhtw/swen3/persistence/entity/RecipientEntity.java
@@ -5,33 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import javax.validation.Constraint;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
 public class RecipientEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
     private Long id;
-    @NotNull(message = "Name cannot be null")
     private String name;
-    @NotNull(message = "Street cannot be null")
-    @Pattern(regexp = "^[A-Za-z]+\s[A-Za-z0-9/]+$")
     private String street;
-    @NotNull(message = "Postal Code cannot be null")
-    @Pattern(regexp = "^A-[0-9]{4}$")
     private String postalCode;
-    @NotNull(message = "City cannot be null")
-    @Pattern(regexp = "^[A-Z][A-Za-z\s0-9-]+$")
     private String city;
-    @NotNull(message = "Country cannot be null")
-    @Pattern(regexp = "^[A-Z][A-Za-z\s0-9-]+$")
     private String country;
     private Long recipientParcel;
     private Long senderParcel;
