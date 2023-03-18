f44558a36952579bf56e135cca6d92ccc522385d
Vasilije Dikic <vaske001@gmail.com>
2023-03-18 20:14:00 +0100

    Add: JSON and Bat files for manual testing

---
+++ springboot/src/main/resources/tests/testcurl_grosses.bat
@@ -0,0 +1,1 @@
+curl -X POST "http://localhost:8080/warehouse" -H  "accept: */*" -H  "Content-Type: application/json" --data-binary "@./grosses_testwarehouse.json"
---
+++ springboot/src/main/resources/tests/testcurl_kleines.bat
@@ -0,0 +1,1 @@
+curl -X POST "http://localhost:8080/warehouse" -H  "accept: */*" -H  "Content-Type: application/json" --data-binary "@./kleines_testwarehouse.json"
